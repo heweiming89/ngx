5eca9b3db7c52332c5c089185df17ac3537e0d6e
czheweiming89 <>
2017-09-23 09:43:44 +0800

    metronic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,28 @@
-import {Component} from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {NavigationEnd, NavigationStart, Router} from '@angular/router';
+import {Helpers} from './helpers';
 
 @Component({
-  selector: 'app-root',
+  selector: 'body',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  encapsulation: ViewEncapsulation.None,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-  heroes = HEROES;
-  selectedHero: Hero;
+  globalBodyClass = 'm-page--loading-non-block m-page--fluid m--skin- m-content--skin-light2 m-header--fixed m-header--fixed-mobile m-aside-left--enabled m-aside-left--skin-dark m-aside-left--offcanvas m-footer--push m-aside--offcanvas-default';
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+  constructor(private _router: Router) {
   }
-}
 
-const HEROES: Hero[] = [
-  {id: 11, name: 'Mr. Nice'},
-  {id: 12, name: 'Narco'},
-  {id: 13, name: 'Bombasto'},
-  {id: 14, name: 'Celeritas'},
-  {id: 15, name: 'Magneta'},
-  {id: 16, name: 'RubberMan'},
-  {id: 17, name: 'Dynama'},
-  {id: 18, name: 'Dr IQ'},
-  {id: 19, name: 'Magma'},
-  {id: 20, name: 'Tornado'}
-];
-
-export class Hero {
-  id: number;
-  name: String;
+  ngOnInit() {
+    this._router.events.subscribe((route) => {
+      if (route instanceof NavigationStart) {
+        Helpers.setLoading(true);
+        Helpers.bodyClass(this.globalBodyClass);
+      }
+      if (route instanceof NavigationEnd) {
+        Helpers.setLoading(false);
+      }
+    });
+  }
 }
